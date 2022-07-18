c6cd8f4d756b41fa395468240857c9826ba03dd9
Naqvi <44436701+nrcool@users.noreply.github.com>
2022-07-18 14:42:43 +0200

    Initialize autograding

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const customer = {
+	first_name: "Bob",
+	last_name: "Dylan"
+};
+
+//              your code inside these <div> tags
+const Output = () => (
+	<div>
+	
+	</div>
+)
+
+export default Output;
