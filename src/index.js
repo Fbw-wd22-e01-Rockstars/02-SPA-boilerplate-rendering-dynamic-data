74c7fa58ad63e27648a1324bb2fc40c49b5b0f17
Naqvi <44436701+nrcool@users.noreply.github.com>
2022-07-18 14:42:44 +0200

    Initialize TW-29-update-react-main

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const customer = {
+  first_name: "Bob",
+  last_name: "Dylan",
+};
+
+//              your code inside these <div> tags
+const output = <div></div>;
+root.render(output);
